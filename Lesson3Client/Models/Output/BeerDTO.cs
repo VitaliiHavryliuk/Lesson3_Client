4487434ca856276128110235b79dca0b124a0c24
VitaliiHavryliuk <107536476+VitaliiHavryliuk@users.noreply.github.com>
2023-02-07 21:02:11 +0200

    Update

---
+++ Lesson3Client/Models/Output/BeerDTO.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Models.Output
+{
+    public class BeerDTO
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+    }
+}
