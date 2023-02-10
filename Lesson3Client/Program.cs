a06975e997a34ab18e338f672e67df9ffc682ecf
VitaliiHavryliuk <107536476+VitaliiHavryliuk@users.noreply.github.com>
2023-02-10 17:17:01 +0200

    Added photots

--- Lesson3Client/Program.cs
+++ Lesson3Client/Program.cs
@@ -1,11 +1,13 @@
 using Lesson3Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Tewr.Blazor.FileReader;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://lesson3apimanagement.azure-api.net/Lesson3API/v1/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? "https://lesson3apimanagement.azure-api.net/Lesson3API/v2/") });
+builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
 
 await builder.Build().RunAsync();
