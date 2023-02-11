6e87a6abf20719bd14dc4b982d093871f6f810d8
VitaliiHavryliuk <107536476+VitaliiHavryliuk@users.noreply.github.com>
2023-02-11 15:44:51 +0200

    Added auth

---
+++ Lesson3Client/Auth/CustomAuthorizationMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace Lesson3Client.Auth
+{
+    public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
+    {
+        public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
+            NavigationManager navigationManager)
+            : base(provider, navigationManager)
+        {
+            ConfigureHandler(
+                authorizedUrls: new[] { "http://localhost:7071/api/" });
+        }
+    }
+}
--- Lesson3Client/Program.cs
+++ Lesson3Client/Program.cs
@@ -1,13 +1,27 @@
 using Lesson3Client;
+using Lesson3Client.Auth;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tewr.Blazor.FileReader;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+builder.Services.AddHttpClient("API_Client", client => client.BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? "https://lesson3apimanagement.azure-api.net/Lesson3API/v2/"))
+    .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("API_Client"));
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? "https://lesson3apimanagement.azure-api.net/Lesson3API/v2/") });
 builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+    options.ProviderOptions.AdditionalScopesToConsent.Add("https://graph.microsoft.com/email");
+    options.ProviderOptions.AdditionalScopesToConsent.Add("https://graph.microsoft.com/profile");
+    options.ProviderOptions.LoginMode = "redirect";
+});
 
 await builder.Build().RunAsync();
