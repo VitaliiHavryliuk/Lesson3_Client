fc73a0820860feaf2df394034473b7d4f05134e3
VitaliiHavryliuk <107536476+VitaliiHavryliuk@users.noreply.github.com>
2023-02-13 18:24:45 +0200

    Revert "Changed app regsiter"
    This reverts commit 42859095e0d3521f3d0ee55068d3517f954c0116.

--- Lesson3Client/Program.cs
+++ Lesson3Client/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = t
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://23e403d5-2302-4b2b-8e61-04464b990ff3/API.Access");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("66d3c2fa-21f9-46e1-9f5c-11327099cd47/API.Access");
     options.ProviderOptions.LoginMode = "redirect";
 });
 
