2ba57825ad3bd50fd642865858915de25387d642
VitaliiHavryliuk <107536476+VitaliiHavryliuk@users.noreply.github.com>
2023-02-13 18:44:08 +0200

    Updated reg

--- Lesson3Client/Auth/CustomAuthorizationMessageHandler.cs
+++ Lesson3Client/Auth/CustomAuthorizationMessageHandler.cs
@@ -11,7 +11,7 @@ namespace Lesson3Client.Auth
             : base(provider, navigationManager)
         {
             ConfigureHandler(
-                authorizedUrls: new[] {"https://lesson3apimanagement.azure-api.net/Lesson3API/v2/" },
+                authorizedUrls: new[] { "http://localhost:7071/api/" ?? "https://lesson3apimanagement.azure-api.net/Lesson3API/v2/" },
                 scopes: new[] { "api://66d3c2fa-21f9-46e1-9f5c-11327099cd47/API.Access" });
         }
     }
